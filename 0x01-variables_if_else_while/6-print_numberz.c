92a1a6ad8c094324509dc65ea721ca21fbce24b4
George Emad <geoem178@gmail.com>
2023-08-11 05:52:26 -0700

    add 1 file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,11 +4,12 @@
  * main - prints all single digit numbers of base 10 starting from 0
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
