daa21dfe879b6d0a2a45d1a97d6834ba4eef1088
George Emad <geoem178@gmail.com>
2023-08-11 05:36:28 -0700

    add 1 file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers from 0 to 9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n=0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
