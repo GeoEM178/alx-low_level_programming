a57db3f536ec93121be9e1c21fadf57d68416b98
George Emad <geoem178@gmail.com>
2023-08-11 05:40:37 -0700

    add 1 file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,15 +4,13 @@
  * main - prints all numbers from 0 to 9
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n=0;
+	int n;
 
-	while (n < 10)
-	{
+	for (n = 0; n < 10; n++)
 		printf("%d", n);
-		n++;
-	};
 	printf("\n");
 	return (0);
 }
