1d3c2fe847ff3edb5ded526e1aa96453543a77a4
GeoEM178 <138987489+GeoEM178@users.noreply.github.com>
2023-09-25 22:19:37 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,25 +1,25 @@
 #include "lists.h"
 
 /**
- * reverse_listint - a linked list reverses
- * @head: first node pointerr
+ * reverse_listint - a linked listReverses
+ * @head: first nodePointer..
  *
  * Return: first node pointer
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prevus = NULL;
+	listint_t *back = NULL;
 	listint_t *next = NULL;
 
 	while (*head)
 	{
 		next = (*head)->next;
-		(*head)->next = prevus;
-		prevus = *head;
+		(*head)->next = back;
+		back = *head;
 		*head = next;
 	}
 
-	*head = prevus;
+	*head = back;
 
 	return (*head);
 }
