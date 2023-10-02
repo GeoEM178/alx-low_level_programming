e1626e5721ce10beca648672ff292cd9a07cccca
GeoEM178 <138987489+GeoEM178@users.noreply.github.com>
2023-10-02 18:05:43 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (lennnl = 0; text_content[len];)
+		for (lennnl = 0; text_content[lennnl];)
 			lennnl++;
 	}
 
