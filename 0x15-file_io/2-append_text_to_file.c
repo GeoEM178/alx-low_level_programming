2b3a88b0f16d68fe47ba7c89122e693ba7ab522f
GeoEM178 <138987489+GeoEM178@users.noreply.github.com>
2023-10-02 18:14:14 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,19 +11,19 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, lennnln = 0;
+	int o, w, ads = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (lennnln = 0; text_content[lennnln];)
-			lennnln++;
+		for (ads = 0; text_content[ads];)
+			ads++;
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, lennnln);
+	w = write(o, text_content, ads);
 
 	if (o == -1 || w == -1)
 		return (-1);
