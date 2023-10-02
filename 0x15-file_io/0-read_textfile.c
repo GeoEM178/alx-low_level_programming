ccec998c67fde3b807bca1a6dbb6a830ba8e5391
GeoEM178 <138987489+GeoEM178@users.noreply.github.com>
2023-10-02 17:57:52 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,37 +2,37 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * read_textfile - See the text value of the file.
+ * @filename: Name of file pointer.
+ * @letters: The letters numbers
+ *           function must read and print.
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * Return: If the function fails or the filename is NULL - 0.
+ *         O/w - the total of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
-	char *buffer;
+	char *nsra;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	nsra = malloc(sizeof(char) * letters);
+	if (nsra == NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	r = read(o, nsra, letters);
+	w = write(STDOUT_FILENO, nsra, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buffer);
+		free(nsra);
 		return (0);
 	}
 
-	free(buffer);
+	free(nsra);
 	close(o);
 
 	return (w);
